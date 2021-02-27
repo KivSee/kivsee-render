4c12f4b373ec3fcbb85e985335a07fda7c0916b4
Oren Bigler <orenbi@altair-semi.com>
2021-02-27 20:50:06 +0200

    changing dir name from hue to effect

--- examples/from_protobuf/pio_deploy.sh
+++ examples/from_protobuf/pio_deploy.sh
@@ -1,4 +1,5 @@
 rm msg.h
 python3 generate_animation.py
-pio ci --board=esp32dev --lib "../../" --project-option="lib_deps=nanopb, NeoPixelBus" --project-option="targets=upload" --project-option="upload_port=/dev/cu.SLAB_USBtoUART" from_protobuf.cpp msg.h
+pio ci --board=esp32dev --lib "../../" --project-option="lib_deps=nanopb, NeoPixelBus" --project-option="targets=upload" from_protobuf.cpp msg.h
+#pio ci --board=esp32dev --lib "../../" --project-option="lib_deps=nanopb, NeoPixelBus" --project-option="targets=upload" --project-option="upload_port=/dev/cu.SLAB_USBtoUART" from_protobuf.cpp msg.h
 #pio ci --board=esp32doit-devkit-v1 --lib "../../" --project-option="lib_deps=nanopb, NeoPixelBus" --project-option="targets=upload" from_protobuf.cpp msg.h
