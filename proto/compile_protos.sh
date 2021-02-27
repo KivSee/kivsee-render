8b41a4e2809336a0fa0c038761049b9bedb309bf
Sapir Caduri <sapir.caduri@gmail.com>
2021-02-27 18:37:31 +0200

    nanopb

--- proto/compile_protos.sh
+++ proto/compile_protos.sh
@@ -1,5 +1,5 @@
-protoc  --plugin=protoc-gen-nanopb=/Users/sapir/nanopb/generator/protoc-gen-nanopb --nanopb_out=. --python_out=. functions.proto effects.proto
-# protoc --nanopb_out=. --python_out=. functions.proto effects.proto
+# protoc  --plugin=protoc-gen-nanopb=/Users/sapir/nanopb/generator/protoc-gen-nanopb --nanopb_out=. --python_out=. --js_out=import_style=commonjs,binary:. functions.proto effects.proto
+protoc --nanopb_out=. --python_out=. functions.proto effects.proto
 
 
 mv functions.pb.c ../src
