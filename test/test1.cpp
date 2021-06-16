4065eb36ec95b3dbd9288f9820febeaafb7701df
Mickey Iluz <mixi.il@gmail.com>
2021-06-16 07:58:19 +0300

    use gtest for test1 example

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,8 +1,30 @@
-#include <cstdio>
+#include <gtest/gtest.h>
 
-int main()
+template <typename T>
+T f_add (T a, T b)
 {
-    printf("test good. really.\n");
-    return 0;
+    return a + b;
+}
+
+template <typename T>
+T f_mul (T a, T b)
+{
+    return a * b;
+}
+
+
+TEST (SillyNumbersTest, Addition) { 
+    EXPECT_EQ (6, f_add(3, 3));
+    EXPECT_EQ (10, f_add(9, 1));
+}
+
+TEST (SillyNumbersTest, Multiplication) { 
+    ASSERT_EQ (0.0, f_mul(0.0, 3.1));
+    ASSERT_EQ (1.0, f_mul(3.1, 1.0/3.1));
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
