4c63e04dc74eb981ae9e3a4b8e8882aeb1534bf0
Jonathan Levin <jon.levin25@gmail.com>
2025-04-21 00:41:10 +0300

    Fix init + rendering

--- unity/NewUnityPlugin.cpp
+++ unity/NewUnityPlugin.cpp
@@ -75,13 +75,13 @@ void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
     Log("KivSeeRender: PluginUnloaded");
 }
 
-void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetAnimationFromPB(const unsigned char* buffer, int bufferSize)
+// Get the total number of LEDs
+int UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API GetTotalLedCount()
 {
-    Log("SetAnimationFromPB");
-    
-    pb_istream_t animation_stream = pb_istream_from_buffer(buffer, bufferSize);
-    void *arg_animation = &animation;
-    ::kivsee_render::DecodeAnimationFromPbStream(&animation_stream, nullptr, &arg_animation);
+    if (segments_map) {
+        return segments_map->number_of_pixels;
+    }
+    return 0;
 }
 
 void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetSegmentsFromPB(const unsigned char* buffer, int bufferSize)
@@ -93,22 +93,55 @@ void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetSegmentsFromPB(const unsigned
     segments_map_decode_args.out_segments_map = &segments_map;
     segments_map_decode_args.leds_array = leds;
     void *arg = &segments_map_decode_args;
-    ::kivsee_render::segments::DecodeSegmentsMapFromPbStream(&segments_map_stream, nullptr, &arg);
-    char print_buf[100];
-    sprintf(print_buf, "guid: %d", segments_map->guid);
-    Log(print_buf);
+    bool success = ::kivsee_render::segments::DecodeSegmentsMapFromPbStream(&segments_map_stream, nullptr, &arg);
+    
+    if (success && segments_map != nullptr) {
+        char print_buf[100];
+        sprintf(print_buf, "guid: %u", segments_map->guid);
+        Log(print_buf);
+        Log("Segments Initialized");
 
-
-    Log("Segments Initialized");
+        sprintf(print_buf, "number of pixels: %u", segments_map->number_of_pixels);
+        Log(print_buf);
+    } else {
+        LogError("Failed to decode segments map or segments_map is null");
+        LogError(segments_map_stream.errmsg);
+    }
 }
 
-// Get the total number of LEDs
-int UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API GetTotalLedCount()
+void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetAnimationFromPB(const unsigned char* buffer, int bufferSize)
 {
-    if (segments_map) {
-        return segments_map->number_of_pixels;
+    Log("SetAnimationFromPB");
+
+    kivsee_render::DecodeAnimationArgs args = { segments_map };
+    void *decodeArgs = &args;
+    
+    pb_istream_t animation_stream = pb_istream_from_buffer(buffer, bufferSize);
+    bool success = ::kivsee_render::DecodeAnimationFromPbStream(&animation_stream, nullptr, &decodeArgs);
+
+    // Turns decodeArgs into animation SOMEHOW
+    animation = (::kivsee_render::Animation *)decodeArgs;
+    
+    if (!success || animation == nullptr) {
+        LogError("Failed to decode animation or animation is null");
+        LogError(animation_stream.errmsg);
+        return;
     }
-    return 0;
+
+    if (animation->duration_ms == 0) {
+        LogError("Invalid animation: duration is 0!");
+        return;
+    }
+
+    char print_buf[100];
+    sprintf(print_buf, "Animation decoded successfully. duration: %u", animation->duration_ms);
+    Log(print_buf);
+}
+
+UNITY_INTERFACE_EXPORT bool UNITY_INTERFACE_API RenderFrame(unsigned long timeMs)
+{
+    animation->Render(timeMs);
+    return true;
 }
 
 // Get the HSV values for all LEDs
