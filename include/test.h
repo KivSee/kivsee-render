de0916772d33f6b2cd80bc69d013caf701653d93
Amir Blum <amirgiraffe@gmail.com>
2025-05-17 13:33:15 +0700

    chore: remove test file changes

--- include/test.h
+++ include/test.h
@@ -1,5 +1,3 @@
 #include "pb_encode.h"
 
 const char *GetNameForTest();
-
-int TestFunction(int x, int y);
--- src/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <test.h>
-
-int TestFunction(int x, int y)
-{
-    return x + y;
-}
